a8949274ade8b7c5f2733e9243a5c001bb86ad9a
kkmonlee <kkmonlee@gmail.com>
2017-07-08 01:18:50 +0100

    New additions now send alert to relative

--- backend/add_contents.php
+++ backend/add_contents.php
@@ -20,4 +20,28 @@
     } else {
         echo "Failed to insert! \n" . mysqli_error($conn) . "\n" . mysqli_errno();
     }
+
+    $message = "Your+relative+is+in+need+of+your+help.+Please+login+in+at+kkmonlee.com+if+you+have+an+account+to+see+the+items+and+pay.+Thanks!";
+    $api_key = "ab13673024f9a6e70578f11169b81e55d89c9a91";
+    $url = "https://api.clockworksms.com/http/send.aspx?key=" . $api_key . "&to=" . $relative_num . "&content=" . $message;
+    
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    //curl_setopt($ch, CURLOPT_HEADER, 0);
+
+    curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        die('Failed to send request ' . curl_error($ch));
+    } else {
+        $result_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($result_status == 200) {
+            // all good
+        } else {
+            die('Request failed: HTTP status code ' . $result_status);
+        }
+    }
+
+    curl_close($ch);
 ?>
